bad4209ee036d9a333543158ed177cd314c04000
Aniket Shaw <85289910+aniketshaw29@users.noreply.github.com>
2023-01-08 23:33:01 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import numpy as np
+from pyzbar.pyzbar import decode
+import cv2
+img=cv2.imread("q2.png")
+code = decode(img)
+print(code)
+for barcode in decode(img):
+    print(barcode.data)
+    text=barcode.data.decode('utf-8')
+    print(text)
+    print(barcode.rect)
+    
+    
+#pip install numpy
+#pip install pyzbar
+#pip install opencv-python    
